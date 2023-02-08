b729cca93d8d592962ec07783a69825f3fdb0372
Pablo Embarach <pabloembarach@gmail.com>
2023-02-08 14:24:26 +0000

    Created routing in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import NavBar from './components/NavBar';
 import style from  './App.module.css';
+import Container from 'react-bootstrap/Container';
+import { Route,Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={style.App}>
       <NavBar />
+      <Container className={style.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() =><h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() =><h1>Sign up</h1>} />
+          <Route render={()=><p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }

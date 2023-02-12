02e863243ba6c7c5df53af5cfd73d6df8a240231
Pablo Embarach <pabloembarach@gmail.com>
2023-02-12 14:30:06 +0000

    update Axios base URL

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://git.heroku.com/dfi-api.git'
+axios.defaults.baseURL = 'https://dfi-api.herokuapp.com/'
 axios.defaults.headers.post["Content-TYpe"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
